6eb68b99e68fd6ddbaf6b153cea601e81a68564c
Robert Maynard <robert.maynard@kitware.com>
2015-07-22 11:30:52 -0400

    Extend detection of msvc up to 2015.

--- builds/cmake/Modules/TestZMQVersion.cmake
+++ builds/cmake/Modules/TestZMQVersion.cmake
@@ -7,12 +7,17 @@ set(ZMQ_VERSION "${ZMQ_VERSION_MAJOR}.${ZMQ_VERSION_MINOR}.${ZMQ_VERSION_PATCH}"
 
 message(STATUS "Detected ZMQ Version - ${ZMQ_VERSION}")
 
-if(MSVC_VERSION MATCHES "1700")
+if(MSVC14)
+  set(_zmq_COMPILER "-v140")
+elseif(MSVC12)
+  set(_zmq_COMPILER "-v120")
+elseif(MSVC11)
   set(_zmq_COMPILER "-v110")
 elseif(MSVC10)
   set(_zmq_COMPILER "-v100")
 elseif(MSVC90)
   set(_zmq_COMPILER "-v90")
-else()
+elseif(NOT _zmq_COMPILER)
+  #only clear if the user hasn't manually specified these
   set(_zmq_COMPILER "")
 endif()
